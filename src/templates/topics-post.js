abc09dcebe4485d50ea7d15ff8c76e6b57953224
blancTo <git@blanc.to>
2023-11-08 03:58:36 +0900

    リンクエラー削除

--- src/templates/topics-post.js
+++ src/templates/topics-post.js
@@ -1,108 +1,104 @@
-import * as React from "react"
-import { Link,graphql } from "gatsby"
+import * as React from 'react';
+import { Link, graphql } from 'gatsby';
 
-import Seo from "../components/Seo"
+import Seo from '../components/Seo';
 
-import Header from "../components/Header"
-import Nav from "../components/Nav"
-import Layout from "../components/Layout"
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import Layout from '../components/Layout';
 import PageTitle from '../components/PageTitle';
 
-import { AnchorLink } from "gatsby-plugin-anchor-links";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faChevronUp } from "@fortawesome/free-solid-svg-icons"
+import { AnchorLink } from 'gatsby-plugin-anchor-links';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
-const siteurl = "https://aishin2484.jp";
+const siteurl = 'https://aishin2484.jp';
 
-export default function TopicsPost({data}) {
+export default function TopicsPost({ data }) {
   return (
     <>
-
       <Header />
 
       <Nav />
 
-      <p id="page-top"><AnchorLink offset="0" to={'/topics/' + data.microcmsTopics.category.slug + '/' + data.microcmsTopics.topicsId + '/#pagetop'} title="Pagetop"><FontAwesomeIcon icon={faChevronUp} /></AnchorLink></p>
+      <p id='page-top'>
+        <AnchorLink offset='0' to={'/topics/' + data.microcmsTopics.category.slug + '/' + data.microcmsTopics.topicsId + '/#pagetop'} title='Pagetop'>
+          <FontAwesomeIcon icon={faChevronUp} />
+        </AnchorLink>
+      </p>
 
-      <section id="page-info" className="topics">
-        <div id="breadcrumb">
-          <ul itemscope itemtype="https://schema.org/BreadcrumbList">
-            <li className='breadcrumb__item' itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-              <a href={siteurl} itemprop="item">
-                  <span itemprop="name">ホーム</span>
+      <section id='page-info' className='topics'>
+        <div id='breadcrumb'>
+          <ul itemscope itemtype='https://schema.org/BreadcrumbList'>
+            <li className='breadcrumb__item' itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+              <a href={siteurl} itemprop='item'>
+                <span itemprop='name'>ホーム</span>
               </a>
-              <meta itemprop="position" content="1" />
-            </li>                    
-            <li>
-              <a href={ siteurl + '/topics/'} itemprop="item">
-                  <span>トピックス</span>
-              </a>
-              <meta itemprop="position" content="2" />
+              <meta itemprop='position' content='1' />
             </li>
             <li>
-              <a href={ siteurl + '/topics/' + data.microcmsTopics.category.slug + '/'} itemprop="item">
+              <a href={siteurl + '/topics/'} itemprop='item'>
+                <span>トピックス</span>
+              </a>
+              <meta itemprop='position' content='2' />
+            </li>
+            <li>
+              <a href={siteurl + '/topics/' + data.microcmsTopics.category.slug + '/'} itemprop='item'>
                 <span>{data.microcmsTopics.category.name}</span>
               </a>
-              <meta itemprop="position" content="3" />
+              <meta itemprop='position' content='3' />
             </li>
             <li>
-              <a href={ siteurl + '/topics/' + data.microcmsTopics.category.slug + '/' + data.microcmsTopics.topicsId + '/' } itemprop="item">
+              <a href={siteurl + '/topics/' + data.microcmsTopics.category.slug + '/' + data.microcmsTopics.topicsId + '/'} itemprop='item'>
                 <span>{data.microcmsTopics.title}</span>
               </a>
-              <meta itemprop="position" content="4" />
+              <meta itemprop='position' content='4' />
             </li>
           </ul>
         </div>
-        <PageTitle
-        title={data.microcmsTopics.category.name}
-        slug={data.microcmsTopics.category.slug}
-        />
+        <PageTitle title={data.microcmsTopics.category.name} slug={data.microcmsTopics.category.slug} />
       </section>
 
       <Layout>
-          
-
-      <section id="main">
-          <h1 id="display-item2">
-              <span className="date">{data.microcmsTopics.date}</span>
-              {data.microcmsTopics.title}
+        <section id='main'>
+          <h1 id='display-item2'>
+            <span className='date'>{data.microcmsTopics.date}</span>
+            {data.microcmsTopics.title}
           </h1>
-          
-          <div id="book">
-            <div id="book-inside">
-              <div id="book-wrap">
+
+          <div id='book'>
+            <div id='book-inside'>
+              <div id='book-wrap'>
                 <div
-                dangerouslySetInnerHTML={{
-                  __html: `${data.microcmsTopics.body}`,
-                }}
+                  dangerouslySetInnerHTML={{
+                    __html: `${data.microcmsTopics.body}`,
+                  }}
                 />
               </div>
             </div>
           </div>
-          <ul className="area_list_link">
-            <li><Link to="/kawagoe/">ペット 火葬 川越市</Link></li>
-            <li><Link to="/tokorozawa/">ペット 火葬 所沢市</Link></li>
-            <li><Link to="/saitama/">ペット 火葬 さいたま市</Link></li>
-          </ul>
-          <p className="center"><Link to={'/topics/' + data.microcmsTopics.category.slug + "/"} className="bt01">一覧へ戻る</Link></p>
-      </section>
-      
+
+          <p className='center'>
+            <Link to={'/topics/' + data.microcmsTopics.category.slug + '/'} className='bt01'>
+              一覧へ戻る
+            </Link>
+          </p>
+        </section>
       </Layout>
     </>
-  )
+  );
 }
 
-
-export const Head = ({data}) => (
+export const Head = ({ data }) => (
   <>
-    <body id="pagetop" />
-    <Seo title={data.microcmsTopics.title + '｜トピックス｜埼玉県のペット火葬・葬儀なら｜愛心ペットセレモ二ー埼玉【公式】'} />    
+    <body id='pagetop' />
+    <Seo title={data.microcmsTopics.title + '｜トピックス｜埼玉県のペット火葬・葬儀なら｜愛心ペットセレモ二ー埼玉【公式】'} />
   </>
-)
+);
 
 export const query = graphql`
-query ($id: String) {
-    microcmsTopics(topicsId: {eq: $id}) {
+  query ($id: String) {
+    microcmsTopics(topicsId: { eq: $id }) {
       topicsId
       title
       date(formatString: "YYYY年MM月DD日")
@@ -115,4 +111,4 @@ query ($id: String) {
       }
     }
   }
-`
+`;
